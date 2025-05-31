ae0ad6fd4779dd94e16d8b9cbb2ff6ca6f642074
Joel P. Rader <echophotographic@gmail.com>
2025-05-31 17:58:12 -0400

    Update cron-hosts-deny.bash
    added backup of hosts.deny staging file

--- cron-hosts-deny.bash
+++ cron-hosts-deny.bash
@@ -3,3 +3,5 @@
 /home/username/deny.pl > /home/username/host-deny-ips.txt
 
 cp /home/username/host-deny-ips.txt /etc/hosts.deny
+
+cp /home/username/host-deny-ips.txt /home/username/hosts.deny.backup.txt
