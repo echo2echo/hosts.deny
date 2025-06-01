6335a49308b9a0a46d5b86ba2adc7954ac63ea07
Joel P. Rader <echophotographic@gmail.com>
2025-06-01 15:17:08 -0400

    Update cron-hosts-deny.bash

--- cron-hosts-deny.bash
+++ cron-hosts-deny.bash
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Staging with deny.pl perl script to merge purge sort IP addresses to block
-# Change paths and filenames to your own preferences  
+# Change paths and filenames to your own preferences 
+
 /home/username/deny/deny.pl > /home/username/deny/host-deny-ips.txt
 cp /home/username/deny/host-deny-ips.txt /etc/hosts.deny
 
-
-# Backup hosts.deny file
 now=`date +"%Y-%m-%d"`
 now="backup-hosts-deny-${now}"
 cp /home/username/deny/host-deny-ips.txt /home/username/deny/${now}
 
-# Backup hosts.allow
 now=`date +"%Y-%m-%d"`
 now="backup-hosts-allow-${now}"
 cp /etc/hosts.allow /home/username/deny/${now}
