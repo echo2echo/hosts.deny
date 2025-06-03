a16b6a9473a797235a08421ea95012c7b36fe863
Joel P. Rader <echophotographic@gmail.com>
2025-06-03 13:43:40 -0400

    Update cron-hosts-deny.bash

--- cron-hosts-deny.bash
+++ cron-hosts-deny.bash
@@ -4,15 +4,15 @@
 # Change paths and filenames to your own preferences 
 # Backups too, below.
 
-/home/username/folder/deny.pl > /home/username/folder/host-deny-ips.txt
-cp /home/username/folder/host-deny-ips.txt /etc/hosts.deny
+/home/username/subfolder/deny.pl > /home/username/subfolder/host-deny-ips.txt
+cp /home/username/subfolder/host-deny-ips.txt /etc/hosts.deny
 
 # Backup hosts.deny 
 now=`date +"%Y-%m-%d"`
 now="backup-hosts-deny-${now}"
-cp /etc/hosts.deny /home/username/folder/${now}
+cp /etc/hosts.deny /home/username/subfolder/${now}
 
 # Backup hosts.allow
 now=`date +"%Y-%m-%d"`
 now="backup-hosts-allow-${now}"
-cp /etc/hosts.allow /home/username/folder/${now}
+cp /etc/hosts.allow /home/username/subfolder/${now}
